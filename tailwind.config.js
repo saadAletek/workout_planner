244410e02dc3e6bbba42b3926ce2f434a2518eb7
Mr_Hamun <said.elatik.602@gmail.com>
2024-09-15 03:50:40 +0300

    install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#D46868',
+        dark : '#232323'
+      },
+    },
+  },
+  plugins: [],
+}
+
